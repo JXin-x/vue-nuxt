937537bb9471de2fa62e12a9a136745febc1b51c
jiangxin101 <jiangxin101@jd.com>
2023-02-19 16:41:14 +0800

    feat: 模拟数据

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>这里是about页面</div>
+</template>
+
+<script>
+export default {
+  name: "about"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div>
+      这里是测试页面一
+    </div>
+    接口返回数据：{{posts}}
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  async asyncData({$axios}){
+    const result = await $axios.get('http://localhost:8000/post')
+    return{
+      posts: result.data
+    }
+  }
 }
 </script>
